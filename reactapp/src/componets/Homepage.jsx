df827a0307e643691ceaa4dcd0923c8e250033a8
Hitesh <montymalik2507@gmail.com>
2026-04-08 15:05:34 +0530

    Updated files

--- reactapp/src/componets/Homepage.jsx
+++ reactapp/src/componets/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Homepage() {
+// function Homepage() {
     // if (loggedin) {
     //     return ( 
     //         <button>Logout</button>
@@ -10,9 +10,37 @@ function Homepage() {
     //         <button>Login</button>
     //     )
     // }
-returnloggedin ? <button>Logout</button> : <button>Login</button>
+// returnloggedin ? <button>Logout</button> : <button>Login</button>
 
   
+// }
+
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Homepage</h1>
+            </div>
+        )
+    }
+    componentDidMount() {
+        console.log("Component did mount")
+    }
+    shouldComponentUpdate(nextProps, nextState  ) {
+        if (nextState.count===1) {
+            return false;
+        }   
+        console.log("Should component update")
+        return true;
+    }   
+    componentDidUpdate() {
+        console.log("Component did update")
+    }
 }
+
 
 export default Homepage
